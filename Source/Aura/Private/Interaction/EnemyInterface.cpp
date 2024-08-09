dbf4b4ec54288981bd6c63ec44f5d809361c36e7
dr-omikron <andrey163.v@yandex.ru>
2024-08-09 16:26:12 +0400

    Final Chapter 1

---
+++ Source/Aura/Private/Interaction/EnemyInterface.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "Interaction/EnemyInterface.h"
+
---
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -0,0 +1,38 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+
+class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
+class IEnemyInterface;
+
+UCLASS()
+class AURA_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	AAuraPlayerController();
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void PlayerTick(float DeltaTime) override;
+	virtual void SetupInputComponent() override;
+private:
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	IEnemyInterface* LastActor;
+	IEnemyInterface* ThisActor;
+	
+	void Move(const FInputActionValue& InputActionValue);
+	void CursorTrace();
+};
